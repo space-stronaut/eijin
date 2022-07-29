6dcffe0d3db63560be4ca9fff033ef75942a0915
Ronald Abel <ronald.hermansyah@kinersia.com>
2022-07-29 22:53:19 +0700

    update

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -14,7 +14,7 @@
                 <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">{{ Auth::user()->name }} - {{ count(App\Models\Status::where('user_id', $item->id)->get()) > 0 ? App\Models\Status::where('user_id', $item->id)->orderBy('updated_at', 'desc')->get()[0]->status == 'masuk' ? 'Aktif' : 'Keluar' : ''}}</a>
+                <a href="#" class="d-block">{{ Auth::user()->name }} - {{ count(App\Models\Status::where('user_id', Auth::user()->id)->get()) > 0 ? App\Models\Status::where('user_id', Auth::user()->id)->orderBy('updated_at', 'desc')->get()[0]->status == 'masuk' ? 'Aktif' : 'Keluar' : ''}}</a>
             </div>
         </div>
 
